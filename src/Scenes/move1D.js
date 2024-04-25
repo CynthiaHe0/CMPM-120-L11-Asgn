b9be27e6d244732107a1b7c4c8fb9f74f18c19f9
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2024-04-24 21:48:07 -0700

    Made the shooting and moving work* *An already flying bullet will despawn if space is pressed too soon

--- src/Scenes/move1D.js
+++ src/Scenes/move1D.js
@@ -15,6 +15,15 @@ class Move1D extends Phaser.Scene{
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "greenBody");
         this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        let spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        my.sprite.gemShot = this.add.sprite(0, 0, "gemShot");
+        my.sprite.gemShot.visible = false;
+        spaceBar.on('down', (key, event) => {
+            console.log("Made it here!");
+            my.sprite.gemShot.x = my.sprite.body.x;
+            my.sprite.gemShot.y = this.bodyY + 10;
+            my.sprite.gemShot.visible = true;
+        });
     }
     update(){
         let my = this.my;
@@ -25,6 +34,11 @@ class Move1D extends Phaser.Scene{
         if (this.dKey.isDown){
             my.sprite.body.x += 1;
             if (my.sprite.body.x >= 800) my.sprite.body.x = 800;
+        }
+        if (my.sprite.gemShot.y > 0){
+            my.sprite.gemShot.y -= 2;
+        } else {
+            my.sprite.gemShot.visible = false;
         }
     }
 }
